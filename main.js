b8701f720b056d7bc04497c212627111d4b33870
medisamanvi <85389353+medisamanvi@users.noreply.github.com>
2022-03-08 18:55:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML=
+        "<img id='captured_img' src='"+data_uri+"'/>"
+    });
+}
+
+console.log(ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/p7SiXvbRg/model.json",modelloaded);
+
+function modelloaded(){
+    console.log("model is loaded")
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data="the first prediction is "+prediction1;
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis)
+}
